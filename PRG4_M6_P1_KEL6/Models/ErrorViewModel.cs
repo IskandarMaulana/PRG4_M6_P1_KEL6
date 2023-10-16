8636c104acf5250f842069da49945bbcbb35ccba
IskandarMaulana <iskandar.maulana0410@gmail.com>
2023-10-16 11:12:49 +0700

    Buat Project

---
+++ PRG4_M6_P1_KEL6/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PRG4_M6_P1_KEL6.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
